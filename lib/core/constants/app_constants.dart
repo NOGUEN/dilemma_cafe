99a99e30ec8b9e6ee77505b57e1413ce3c078f15
NOGUEN <nhg1113@naver.com>
2024-06-16 16:26:26 +0900

    feat: 매직넘버 제거를 위한 AppValues 추가

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,4 +1,11 @@
 class RoutePaths {
   static const String Home = '/';
   static const String Login = 'login';
+  static const String Dillema = 'dillema';
+}
+
+class AppValues {
+  static const double horizontalPadding = 20.0;
+  static const double space = 15.0;
+  static const double boxBorderRadius = 8.0;
 }
