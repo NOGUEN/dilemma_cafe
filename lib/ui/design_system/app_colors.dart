fd838f4f96b0825525eed73780658029c56b45f2
NOGUEN <nhg1113@naver.com>
2024-06-17 01:44:52 +0900

    feat: 색상 수정

--- lib/ui/design_system/app_colors.dart
+++ lib/ui/design_system/app_colors.dart
@@ -7,8 +7,8 @@ class AppColors {
   static const Color black = Color(0xFF000000);
   static const Color white = Color(0xFFFFFFFF);
 
-  static const Color gray = Color(0xFFACACAC);
-  static const Color lightGray = Color(0xFFF6F6F6);
+  static const Color gray = Color(0xFF5B5B5B);
+  static const Color lightGray = Color(0xFFACACAC);
   static const Color darkGray = Color(0xFF2F2F2F);
 
   static const Color kakao = Color(0xFFFFE500);
