81a9be66f6b21935cdf03eeea95e0910ceeb5585
NOGUEN <nhg1113@naver.com>
2024-06-16 17:38:11 +0900

    feat: 딜레마 뷰모델 추가

---
+++ lib/core/viewmodels/dillema_viewmodel.dart
@@ -0,0 +1,51 @@
+import 'package:dillema_cafe/core/viewmodels/base_viewmodel.dart';
+
+class DillemaViewModel extends BaseModel {
+  // HomeViewModel({
+  //   required this.postApi,
+  // });
+
+  // final PostApi postApi;
+
+  // List<Post> posts = <Post>[];
+
+  List<bool> chosenDillema = [false, false];
+
+  void pickDillema(int index) {
+    if (index == 0) {
+      chosenDillema[0] = true;
+      chosenDillema[1] = false;
+    } else {
+      chosenDillema[0] = false;
+      chosenDillema[1] = true;
+    }
+  }
+
+  bool checkSubmitAvailable() {
+    if (chosenDillema.any((element) => element == true) == false) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  @override
+  Future<void> initModel() async {
+    setBusy(true);
+
+    await fetchPosts();
+
+    setBusy(false);
+  }
+
+  Future<void> fetchPosts() async {
+    // final HttpResponse<List<Post>> response = await postApi.getPosts();
+
+    // final int responseCode = response.response.statusCode ?? 0;
+    // if (responseCode < 200 || responseCode > 299) {
+    //   return;
+    // }
+
+    // posts = response.data;
+  }
+}
