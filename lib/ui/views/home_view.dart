d799742ceb2f415bcbc9f12a5d4ddbd5acd9e81c
NOGUEN <nhg1113@naver.com>
2024-06-16 18:15:42 +0900

    feat: 홈 뷰의 메인 기능 버튼에 로고 추가

--- lib/ui/views/home_view.dart
+++ lib/ui/views/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:dillema_cafe/core/constants/app_constants.dart';
 import 'package:dillema_cafe/ui/design_system/font_styles.dart';
 import 'package:dillema_cafe/ui/widgets/dillema_list_cell.dart';
 import 'package:flutter/material.dart';
@@ -5,6 +6,7 @@ import 'package:dillema_cafe/ui/design_system/app_colors.dart';
 import 'package:dillema_cafe/ui/widgets/main_cell.dart';
 import 'package:dillema_cafe/core/viewmodels/home_viewmodel.dart';
 import 'package:dillema_cafe/ui/widgets/base_widget.dart';
+import 'package:flutter_svg/svg.dart';
 
 class HomeView extends StatelessWidget {
   @override
@@ -45,16 +47,24 @@ class HomeView extends StatelessWidget {
                       MainCell(
                         titleText: "오늘의 딜레마",
                         descriptionText: "오늘의 추천 딜레마를 확인해봐요!",
-                        bottomImg: const Icon(Icons.abc),
+                        bottomImg: SvgPicture.asset(
+                          "assets/image/dillema_icon.svg",
+                          width: 60,
+                        ),
                         backgroundColor: AppColors.primary,
                         height: mainCellHeight,
-                        onTapFunction: () {},
+                        onTapFunction: () {
+                          Navigator.of(context).pushNamed(RoutePaths.Dillema);
+                        },
                       ),
                       const SizedBox(width: 20),
                       MainCell(
                         titleText: "딜레마\n전체 보기",
                         descriptionText: "지금까지 올라온 모든 딜레마를 확인해봐요!",
-                        bottomImg: const Icon(Icons.abc),
+                        bottomImg: SvgPicture.asset(
+                          "assets/image/dillema_list_icon.svg",
+                          width: 40,
+                        ),
                         backgroundColor: AppColors.primary,
                         height: mainCellHeight,
                         onTapFunction: () {},
