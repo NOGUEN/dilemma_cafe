f9ed77f8abbad1ce7277c80dc974eac35679a7fa
NOGUEN <nhg1113@naver.com>
2024-06-16 16:30:57 +0900

    feat: 딜레마 카페 공용 버튼 구현

---
+++ lib/ui/widgets/dillema_cafe_button.dart
@@ -0,0 +1,44 @@
+import 'package:dillema_cafe/core/constants/app_constants.dart';
+import 'package:dillema_cafe/ui/design_system/app_colors.dart';
+import 'package:dillema_cafe/ui/design_system/font_styles.dart';
+import 'package:flutter/material.dart';
+
+class DillemaCafeButton extends StatefulWidget {
+  const DillemaCafeButton({
+    super.key,
+    required this.isAvailable,
+    required this.height,
+    required this.titleText,
+    required this.onTapFunction,
+  });
+
+  final bool isAvailable;
+  final double height;
+  final String titleText;
+  final void Function() onTapFunction;
+
+  @override
+  State<DillemaCafeButton> createState() => _DillemaCafeButtonState();
+}
+
+class _DillemaCafeButtonState extends State<DillemaCafeButton> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: widget.onTapFunction,
+      child: Container(
+        height: widget.height,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(AppValues.boxBorderRadius),
+          color: widget.isAvailable ? AppColors.primary : AppColors.darkGray,
+        ),
+        child: Text(
+          widget.titleText,
+          style: AppTextStyles.dillemaDisplay.copyWith(
+            color: AppColors.black,
+          ),
+        ),
+      ),
+    );
+  }
+}
